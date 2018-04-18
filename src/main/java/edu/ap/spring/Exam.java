06854c62e973eb280deb4e71b4b8059769fe645e
Vincent Maes <32563747+VincentMaes26@users.noreply.github.com>
2018-04-18 16:56:01 +0200

    added other exercises

--- src/main/java/edu/ap/spring/Exam.java
+++ src/main/java/edu/ap/spring/Exam.java
@@ -18,18 +18,23 @@ public class Exam {
 		}
 		return numberList;
 	}
-	
+	public List<Point> pointList(){
+		List<Point> pList = new ArrayList<Point>();
+		pList.add(new Point(1, 2));
+		pList.add(new Point(60, 50));
+		pList.add(new Point(50, 3));
+		return pList;
+	}
 	// Maak gebruik van lambdas en streams om een array met alle
 	// priemgetallen terug te geven
 	// 2 punten
-	public Stream<Integer> getPrimes(int[] numbers) {
+	public int[] getPrimes(int[] numbers) {
 		List<Integer> nList = numberList();
 		Stream<Integer> primes = nList
 			.stream()
-			.filter(n -> n%2 != 0);
-			
-		
-		return primes;
+			.filter(n -> n%2 != 0)
+		int[] output = null;
+		return output;
 	}
 	
 	// Maak gebruik van lambdas en streams alle lowercase characters
@@ -51,7 +56,12 @@ public class Exam {
 	// x-coordinaten uit de lijst van points te berekenen
 	// 1 punt
 	public int sumOfX(List<Point> points) {
+		List<Point> pList = pointList();
 		
+		pList
+			.stream();
+		int amount = (int) pList.size();
+		return amount;
 	}
 	
 	// Maak gebruik van lambdas en streams om een comma-separated
@@ -59,6 +69,12 @@ public class Exam {
 	// zijn aan twee
 	// 2 punten
 	public String getXOverTwo(List<Point> points) {
+		List<Point> pList = pointList();
 		
+		pList.stream()
+		.filter((x) -> x.getX() >=2);
+		String output = pList.toString();
+		
+		return output;
 	}
 }
