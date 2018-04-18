77d48fc58c15849302d5a6c3ffac4727055a4b53
Vincent Maes <32563747+VincentMaes26@users.noreply.github.com>
2018-04-18 16:40:57 +0200

    Solved exercise 1 + 2

--- src/main/java/edu/ap/spring/Exam.java
+++ src/main/java/edu/ap/spring/Exam.java
@@ -2,23 +2,49 @@ package edu.ap.spring;
 
 import java.awt.Point;
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 import org.springframework.stereotype.Component;
 
 @Component
 public class Exam {
 	
+	public List<Integer> numberList() {
+		List<Integer> numberList = new ArrayList<Integer>();
+		for(int i = 0; i<20; i++ ) {
+			numberList.add(i);
+		}
+		return numberList;
+	}
+	
 	// Maak gebruik van lambdas en streams om een array met alle
 	// priemgetallen terug te geven
 	// 2 punten
-	public int[] getPrimes(int[] numbers) {
+	public Stream<Integer> getPrimes(int[] numbers) {
+		List<Integer> nList = numberList();
+		Stream<Integer> primes = nList
+			.stream()
+			.filter(n -> n%2 != 0);
+			
 		
+		return primes;
 	}
 	
 	// Maak gebruik van lambdas en streams alle lowercase characters
 	// te tellen in de gegeven string
 	// 1 punt
 	public int countLowercaseCharacters(String string) {
+        string = "aklfajAAAklkjfB";
+        char[] charArray = string.toCharArray();
+        Stream<Character> charStream = new String(charArray)
+        		.chars()
+        		.mapToObj(i->(char)i)
+        		.filter(c -> Character.isLowerCase(c));
+        int amount = (int) charStream.count();	
         
+        return amount;
 	}
 	
 	// Maak gebruik van lambdas en streams om de som van alle
